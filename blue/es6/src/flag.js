bb5dc1f17bd87ad76cf357fb3965dc42c1ee2052
ted.song <ted.song@daumkakao.com>
2019-02-05 23:32:18 +0900

    .

--- blue/es6/src/flag.js
+++ blue/es6/src/flag.js
@@ -1,6 +1,7 @@
 export class Flag {
     constructor() {
         this.code = null;
+        this.$element = null;
     }
 
     getCode() {
@@ -10,6 +11,22 @@ export class Flag {
     setCode(code) {
         this.code = code;
         return this;
+    }
+
+    render() {
+        this.$element = $('<div></div>');
+        this.$element.css({
+            position: 'absolute',
+            left: config.flag.left,
+            top: config.flag.top,
+            width: config.flag.width,
+            height: config.flag.height,
+            border: '1px solid black',
+            backgroundImage: `url(../image/${code}.png)`,
+            backgroundSize: `${config.flag.width}px ${config.flag.height}px`
+        });
+
+        return this.$element;
     }
 
     static of() {
