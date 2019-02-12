76cf343775d614cd2eac09cc6bf0c5516464182e
ted.song <ted.song@daumkakao.com>
2019-02-12 22:42:00 +0900

    .

--- blue/vue/special.js
+++ blue/vue/special.js
@@ -2,18 +2,18 @@ Vue.component('special', {
     props: ['index'],
     template: `
         <div v-bind:style="getSpecialStyle()">
-            <span v-bind:style="nameStyle">{{place.name}}</span>
+            <span v-bind:style="getNameStyle()">{{place.name}}</span>
         </div>
     `,
     data: function () {
         return {
-            place: config.placeList[this.index],
-            nameStyle: {
-                backgroundColor: config.selectedColor
-            }
+            place: config.placeList[this.index]
         }
     },
     methods: {
+        getNameStyle() {
+            backgroundColor: config.selectedColor
+        },
         getBackgroundImage() {
             return `url(../image/${this.place.code}.png)`;
         },
