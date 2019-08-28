8faea255a0b5023bb99d3ee20b043debe63e4346
uuidcode <uuidcode@gmail.com>
2019-08-28 13:43:51 +0900

    .

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args){
+        Date date = new Date(1561103876153L);
+        System.out.println(date);
+    }
+}
