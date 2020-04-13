421ddb473f0d90178810f597a4ce536929025147
ted.song <ted.song@daumkakao.com>
2020-04-13 14:54:27 +0900

    .

--- aab/aab.js
+++ aab/aab.js
@@ -28,8 +28,8 @@ var app = new Vue({
             {
                 styleObject: {
                     position: 'absolute',
-                    left: '600px',
-                    top: '200px',
+                    left: '575px',
+                    top: '250px',
                     width: '100px',
                     height: '100px',
                     backgroundImage: 'url(image/1.png)'
@@ -195,46 +195,50 @@ var app = new Vue({
                 }
             },
             {
+                index: 7,
                 styleObject: {
                     position: 'absolute',
-                    left: '800px',
-                    top: '900px',
-                    width: '100px',
-                    height: '100px',
+                    left: '1150px',
+                    top: '650px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 }
             },
             {
+                index: 8,
                 styleObject: {
                     position: 'absolute',
-                    left: '800px',
-                    top: '1000px',
-                    width: '100px',
-                    height: '100px',
+                    left: '1150px',
+                    top: '700px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 }
             },
             {
+                index: 9,
                 styleObject: {
                     position: 'absolute',
-                    left: '800px',
-                    top: '1100px',
-                    width: '100px',
-                    height: '100px',
+                    left: '1150px',
+                    top: '750px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 }
             },
             {
+                index: 10,
                 title: '도둑은 위치 이동을 한다.',
                 styleObject: {
                     position: 'absolute',
-                    left: '1100px',
-                    top: '600px',
-                    width: '100px',
-                    height: '100px',
+                    left: '1600px',
+                    top: '500px',
+                    width: '150px',
+                    height: '50px',
                     backgroundColor: 'red',
                     color: 'white',
                     textAlign: 'center',
@@ -245,136 +249,177 @@ var app = new Vue({
                 trick: true
             },
             {
+                index: 11,
                 styleObject: {
                     position: 'absolute',
-                    left: '1100px',
-                    top: '500px',
-                    width: '100px',
-                    height: '100px',
+                    left: '1600px',
+                    top: '450px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 }
             },
             {
+                index: 12,
                 styleObject: {
                     position: 'absolute',
-                    left: '1100px',
+                    left: '1600px',
                     top: '400px',
-                    width: '100px',
-                    height: '100px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 },
                 trick: true
             },
             {
+                index: 13,
                 title: '멈춤',
                 subTitle: '주사위 수가 남아도 반드시 멈춘다',
                 styleObject: {
                     position: 'absolute',
-                    left: '1000px',
+                    left: '1450px',
                     top: '400px',
-                    width: '100px',
-                    height: '100px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 }
             },
             {
+                index: 14,
                 styleObject: {
                     position: 'absolute',
-                    left: '900px',
+                    left: '1300px',
                     top: '400px',
-                    width: '100px',
-                    height: '100px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 }
             },
             {
+                index: 15,
                 styleObject: {
                     position: 'absolute',
-                    left: '800px',
+                    left: '1150px',
                     top: '400px',
-                    width: '100px',
-                    height: '100px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 },
                 trick: true
             },
             {
+                index: 16,
                 styleObject: {
                     position: 'absolute',
-                    left: '1000px',
-                    top: '600px',
-                    width: '100px',
-                    height: '100px',
+                    left: '1450px',
+                    top: '500px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 }
             },
             {
+                index: 17,
                 title: '보석을 떨어뜨렸다',
                 subTitle: '도둑이 보석을 가지고 있으면 경찰에게 주고 경찰은 다시 숨긴다.',
                 styleObject: {
                     position: 'absolute',
-                    left: '900px',
-                    top: '600px',
-                    width: '100px',
-                    height: '100px',
+                    left: '1300px',
+                    top: '500px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
-                    border: '1px solid black'
+                    border: '1px solid black',
+                    fontSize: '12px'
                 }
             },
             {
+                index: 18,
                 styleObject: {
                     position: 'absolute',
-                    left: '800px',
-                    top: '600px',
-                    width: '100px',
-                    height: '100px',
+                    left: '1150px',
+                    top: '500px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 }
             },
             {
+                index: 19,
                 title: '주민신고!!',
                 subTitle: '경찰은 도둑의 1칸 앞으로 간다.',
                 styleObject: {
                     position: 'absolute',
-                    left: '700px',
+                    left: '1000px',
                     top: '400px',
-                    width: '100px',
-                    height: '100px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     backgroundColor: 'skyblue',
                     border: '1px solid black'
                 }
             },
             {
+                index: 20,
                 styleObject: {
                     position: 'absolute',
-                    left: '600px',
+                    left: '850px',
                     top: '400px',
-                    width: '100px',
-                    height: '100px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 },
                 trick: true
             },
             {
+                index: 21,
+                title: '화장실에 가고 싶다.',
+                subTitle: '도둑은 아지트로 경찰은 감옥으로',
                 styleObject: {
                     position: 'absolute',
-                    left: '600px',
-                    top: '300px',
-                    width: '100px',
-                    height: '100px',
+                    left: '700px',
+                    top: '400px',
+                    width: '150px',
+                    height: '50px',
+                    textAlign: 'center',
+                    border: '1px solid black',
+                    fontSize: '12px'
+                },
+                comeBackHome: true
+            },
+            {
+                index: 22,
+                styleObject: {
+                    position: 'absolute',
+                    left: '550px',
+                    top: '400px',
+                    width: '150px',
+                    height: '50px',
+                    textAlign: 'center',
+                    border: '1px solid black'
+                },
+                trick: true
+            },
+            {
+                index: 23,
+                styleObject: {
+                    position: 'absolute',
+                    left: '550px',
+                    top: '350px',
+                    width: '150px',
+                    height: '50px',
                     textAlign: 'center',
                     border: '1px solid black'
                 }
-            }
+            },
         ]
     }
 });
