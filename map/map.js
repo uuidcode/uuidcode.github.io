f4ee9c16d9af652fd15a55824358122abd1a7297
ted.song <ted.song@daumkakao.com>
2020-05-31 22:17:07 +0900

    .

--- map/map.js
+++ map/map.js
@@ -1,3 +1,5 @@
+const OFFSET = 25;
+
 let data = {
     blockList: [
         {
@@ -86,6 +88,30 @@ let data = {
     ]
 };
 
+function getLinkList(item) {
+    return [
+        [item.styleObject.left + OFFSET, item.styleObject.top],
+        [item.styleObject.left + 2 * OFFSET, item.styleObject.top + OFFSET],
+        [item.styleObject.left + OFFSET, item.styleObject.top + 2 * OFFSET],
+        [item.styleObject.left, item.styleObject.top + OFFSET]
+    ];
+}
+
+function getCurvedArrow(item, nextItem) {
+    let itemLinkList = getLinkList(item);
+    let nextItemLinkList = getLinkList(nextItem);
+
+    console.log('>>> itemLinkList', itemLinkList);
+    console.log('>>> nextItemLinkList', nextItemLinkList);
+
+    return itemLinkList.flatMap(x => {
+        return nextItemLinkList.map(y => {
+            return [...x, ...y, Math.pow(x[0] - y[0], 2) + Math.pow(x[1] - y[1], 2)];
+        })
+    })
+    .sort((x, y) => x[4] - y[4]);
+}
+
 data.blockList.forEach((item) => {
     item.classObject = {
         ...item.classObject,
@@ -94,20 +120,21 @@ data.blockList.forEach((item) => {
 
     item.linkList.forEach((nextItemIndex) => {
         let nextItem = data.blockList[nextItemIndex];
+        let curvedArrow = getCurvedArrow(item, nextItem);
 
-        let offset = 25;
+        console.log('>>> curvedArrow', curvedArrow);
 
+        curvedArrow = curvedArrow[0];
         $(document.body).curvedArrow({
-            p0x: item.styleObject.left + offset,
-            p0y: item.styleObject.top + offset,
-            p1x: item.styleObject.left + offset,
-            p1y: item.styleObject.top + offset,
-            p2x: nextItem.styleObject.left + offset,
-            p2y: nextItem.styleObject.top + offset,
+            p0x: curvedArrow[0],
+            p0y: curvedArrow[1],
+            p1x: curvedArrow[0],
+            p1y: curvedArrow[1],
+            p2x: curvedArrow[2],
+            p2y: curvedArrow[3],
             strokeStyle: 'rgba(133, 255, 133, 1)',
             lineWidth: 5,
             size: 15
-
         });
     });
 });
