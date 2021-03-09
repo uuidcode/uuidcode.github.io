9c1c24974f58895353cd57edef09ab73de94e3ba
ted(송성균) <ted.song@kakaocorp.com>
2021-03-09 21:59:39 +0900

    .

---
+++ katan/src/cardStore.js
@@ -0,0 +1,27 @@
+import { shuffle } from "./util";
+import {writable} from "svelte/store";
+
+let cardList = [];
+
+for (let i = 0; i < 5; i++) {
+    cardList.push('point')
+}
+
+for (let i = 0; i < 14; i++) {
+    cardList.push('knight')
+}
+
+for (let i = 0; i < 2; i++) {
+    cardList.push('road');
+    cardList.push('resource');
+    cardList.push('get');
+}
+
+cardList = shuffle(cardList);
+
+const { subscribe, set, update } = writable(cardList);
+
+export default {
+    subscribe,
+    set
+}
