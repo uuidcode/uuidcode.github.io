67c2adb44f568ccab5157c8c1f0e0039015ea8c4
uuidcode <uuidcode@gmail.com>
2023-10-21 20:50:33 +0900

    .

--- src/main/java/TransparentPanel.java
+++ src/main/java/TransparentPanel.java
@@ -2,21 +2,36 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 import static java.awt.event.KeyEvent.VK_ESCAPE;
 
 public class TransparentPanel extends JPanel implements KeyListener, MouseListener, MouseMotionListener {
     Point stratPoint;
     Point endPoint;
+    JTabbedPane tabbedPane = new JTabbedPane();
+    JFrame frame;
+
+    public TransparentPanel(JFrame frame) {
+        this.frame = frame;
+    }
+
     public void keyPressed(KeyEvent e) {
         printEventInfo("Key Pressed", e);
 
@@ -56,8 +71,40 @@ public class TransparentPanel extends JPanel implements KeyListener, MouseListen
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        stratPoint = null;
-        endPoint = null;
+        try {
+            Rectangle rectangle = this.getRectangle();
+            this.stratPoint = null;
+            this.endPoint = null;
+
+            rectangle.width++;
+            rectangle.height++;
+            this.repaint(rectangle);
+
+            BufferedImage image = new Robot().createScreenCapture(rectangle);
+            String fileName = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
+            File file = new File("screenshot/" + fileName + ".png");
+            ImageIO.write(image, "png", file);
+
+            JPanel panel = new JPanel() {
+                public void paint(Graphics g) {
+                    super.paint(g);
+
+                    try {
+                        BufferedImage bufferedImage = ImageIO.read(file);
+                        g.drawImage(bufferedImage, 0, 0, bufferedImage.getWidth(),
+                            bufferedImage.getHeight(), this);
+                    } catch (Throwable ignored) {
+                    }
+                }
+            };
+
+            tabbedPane.add(fileName, panel);
+            frame.setContentPane(tabbedPane);
+            frame.revalidate();
+            frame.repaint();
+            frame.setAlwaysOnTop(false);
+        } catch (Throwable ignored) {
+        }
     }
 
     @Override
@@ -81,36 +128,45 @@ public class TransparentPanel extends JPanel implements KeyListener, MouseListen
 
     }
 
+    public Rectangle getRectangle() {
+        int x = Math.min(stratPoint.x, endPoint.x);
+        int y = Math.min(stratPoint.y, endPoint.y);
+        int width = Math.abs(stratPoint.x - endPoint.x);
+        int height = Math.abs(stratPoint.y - endPoint.y);
+
+        return new Rectangle(x, y, width, height);
+    }
+
     public void paint(Graphics g) {
         super.paint(g);
-        System.out.println(">>> " + g);
+
         if (stratPoint != null) {
-            int x = Math.min(stratPoint.x, endPoint.x);
-            int y = Math.min(stratPoint.y, endPoint.y);
-            int width = Math.abs(stratPoint.x - endPoint.x);
-            int height = Math.abs(stratPoint.y - endPoint.y);
+            Rectangle rectangle = this.getRectangle();
             g.setColor(Color.black);
-            g.drawRect(x, y, width, height);
-            System.out.println(g);
+            g.drawRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
         }
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Hello");
+        JFrame.setDefaultLookAndFeelDecorated(true);
+//        frame.setBackground(new Color(0, 0, 0, 0));
         frame.setDefaultCloseOperation(javax.swing.WindowConstants.HIDE_ON_CLOSE);
-        frame.setUndecorated(true);
+//        frame.setUndecorated(true);
         frame.setAlwaysOnTop(true);
-        frame.setType(javax.swing.JFrame.Type.UTILITY);
+        frame.setType(JFrame.Type.NORMAL);
         frame.getRootPane().putClientProperty("apple.awt.draggableWindowBackground", false);
         frame.setLocation(0, 0); // 1620 780
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setSize(screenSize);
-        TransparentPanel contentPane = new TransparentPanel();
+
+        TransparentPanel contentPane = new TransparentPanel(frame);
         frame.addKeyListener(contentPane);
         contentPane.addMouseListener(contentPane);
         contentPane.addMouseMotionListener(contentPane);
+
         frame.setContentPane(contentPane);
-        frame.setBackground(new Color(0, 0, 0, 0));
+
         frame.setVisible(true);
     }
 }
