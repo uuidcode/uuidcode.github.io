7dc3c7a99855442161359d5201678266ed2f9120
uuidcode <uuidcode@gmail.com>
2023-11-17 09:44:45 +0900

    .

--- src/main/java/screen/Mode.java
+++ src/main/java/screen/Mode.java
@@ -1,5 +1,6 @@
 package screen;
 
+import java.awt.AlphaComposite;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -7,11 +8,11 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
 
+import static java.awt.AlphaComposite.SRC_OVER;
 import static java.awt.BasicStroke.CAP_BUTT;
 import static java.awt.BasicStroke.JOIN_MITER;
 import static java.awt.RenderingHints.KEY_ANTIALIASING;
 import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
-import static screen.Util.drawArrowLine;
 import static screen.Util.getRectangle;
 
 public enum Mode implements Drawable {
@@ -23,8 +24,10 @@ public enum Mode implements Drawable {
 
             g2.setColor(new Color(124, 166, 208, 244));
             g2.setStroke(new BasicStroke(10, CAP_BUTT, JOIN_MITER));
-            g2.drawLine(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
-            drawArrowLine(g, startPoint.x, startPoint.y, endPoint.x, endPoint.y, 20, 20);
+            setAlphaComposite(g2);
+            Util.drawArrow(g2, startPoint, endPoint,
+                new BasicStroke(10, CAP_BUTT, JOIN_MITER),
+                new BasicStroke(10, CAP_BUTT, JOIN_MITER), 50);
         }
     },
     FILL_RECTANGLE {
@@ -35,6 +38,7 @@ public enum Mode implements Drawable {
 
             g2.setColor(new Color(124, 166, 208, 255));
             g2.setStroke(new BasicStroke(10, CAP_BUTT, JOIN_MITER));
+            setAlphaComposite(g2);
             Rectangle rectangle = getRectangle(startPoint, endPoint);
             g2.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
         }
@@ -47,8 +51,14 @@ public enum Mode implements Drawable {
 
             g2.setColor(new Color(124, 166, 208, 255));
             g2.setStroke(new BasicStroke(3, CAP_BUTT, JOIN_MITER));
+            setAlphaComposite(g2);
             Rectangle rectangle = getRectangle(startPoint, endPoint);
             g2.drawRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
         }
+    };
+
+    private static void setAlphaComposite(Graphics2D g2) {
+        AlphaComposite alphaComposite = AlphaComposite.getInstance(SRC_OVER, 0.5f);
+        g2.setComposite(alphaComposite);
     }
 }
--- src/main/java/screen/Util.java
+++ src/main/java/screen/Util.java
@@ -1,8 +1,12 @@
 package screen;
 
-import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Stroke;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
@@ -19,16 +23,48 @@ public class Util {
         return new File(SCREENSHOT_DIR + name + ".png");
     }
 
-    public static void drawArrowLine(Graphics g, int x0, int y0, int x1,
-                                      int y1, int headLength, int headAngle) {
-        double offs = headAngle * Math.PI / 180.0;
-        double angle = Math.atan2(y0 - y1, x0 - x1);
-        int[] xs = { x1 + (int) (headLength * Math.cos(angle + offs)), x1,
-            x1 + (int) (headLength * Math.cos(angle - offs)) };
-        int[] ys = { y1 + (int) (headLength * Math.sin(angle + offs)), y1,
-            y1 + (int) (headLength * Math.sin(angle - offs)) };
-        g.drawLine(x0, y0, x1, y1);
-        g.drawPolyline(xs, ys, 3);
+    public static void drawArrow(final Graphics2D gfx,
+                                 final Point2D start,
+                                 final Point2D end,
+                                 final Stroke lineStroke,
+                                 final Stroke arrowStroke,
+                                 final float arrowSize) {
+
+        final double startx = start.getX();
+        final double starty = start.getY();
+
+        gfx.setStroke(arrowStroke);
+        final double deltax = startx - end.getX();
+        final double result;
+
+        if (deltax == 0.0d) {
+            result = Math.PI / 2;
+        }
+        else {
+            result = Math.atan((starty - end.getY()) / deltax) + (startx < end.getX() ? Math.PI : 0);
+        }
+
+        final double angle = result;
+
+        final double arrowAngle = Math.PI / 12.0d;
+
+        final double x1 = arrowSize * Math.cos(angle - arrowAngle);
+        final double y1 = arrowSize * Math.sin(angle - arrowAngle);
+        final double x2 = arrowSize * Math.cos(angle + arrowAngle);
+        final double y2 = arrowSize * Math.sin(angle + arrowAngle);
+
+        final double cx = arrowSize /1.05d * Math.cos(angle);
+        final double cy = arrowSize /1.05d * Math.sin(angle);
+
+        final GeneralPath polygon = new GeneralPath();
+        polygon.moveTo(end.getX(), end.getY());
+        polygon.lineTo(end.getX() + x1, end.getY() + y1);
+        polygon.lineTo(end.getX() + x2, end.getY() + y2);
+        polygon.closePath();
+
+        gfx.fill(polygon);
+        gfx.setStroke(lineStroke);
+        gfx.draw(new Line2D.Double(startx, starty, end.getX() + cx, end.getY() + cy));
     }
 
     public static Rectangle getRectangle(Point startPoint, Point endPoint) {
