1ced73d2e4337f4a3b148259a47e89a280fd4b3c
uuidcode <uuidcode@gmail.com>
2023-11-28 22:36:10 +0900

    .

--- imhotep/src/Boat.svelte
+++ imhotep/src/Boat.svelte
@@ -6,9 +6,24 @@
 
     let stoneList = null;
 
+    const handleSortItem = e => {
+        const target = $items.find(item => item.id === e.item.id)
+        const allItems = $items.filter(item => item.id !== e.item.id)
+        const _items = allItems.filter(item => item.boardId === e.to.id)
+        target.boardId = e.to.id
+        _items.splice(e.newIndex, 0, target)
+
+        const newItems = allItems
+            .filter(item => item.boardId !== e.to.id)
+            .concat(_items)
+        items.set(newItems)
+    }
+
     afterUpdate(() => {
         if (stoneList) {
-            new Sortable(stoneList);
+            new Sortable(stoneList, {
+                onEnd: handleSortItem
+            });
         }
     })
 </script>
