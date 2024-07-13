96e245ccf8c412a8401efe9f31f29975b4bd23bb
uuidcode <uuidcode@gmail.com>
2024-07-13 22:09:11 +0900

    This is where it all begins...

--- src/main/java/screen/ThumbnailDialog.java
+++ src/main/java/screen/ThumbnailDialog.java
@@ -10,8 +10,12 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -21,11 +25,17 @@ import javax.swing.JScrollPane;
 
 import static java.awt.BorderLayout.CENTER;
 import static java.awt.BorderLayout.SOUTH;
+import static java.awt.Image.SCALE_SMOOTH;
 import static java.util.Comparator.comparing;
+import static javax.swing.BorderFactory.createCompoundBorder;
+import static javax.swing.BorderFactory.createEmptyBorder;
+import static javax.swing.BorderFactory.createEtchedBorder;
+import static javax.swing.BoxLayout.Y_AXIS;
 import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS;
 
 public class ThumbnailDialog extends JDialog {
     private ImageTabPanel imageTabPanel;
+    private JPanel contentPanel;
 
     public ThumbnailDialog(Frame owner, ImageTabPanel imageTabPanel, String title) {
         super(owner, title, true);
@@ -40,8 +50,10 @@ public class ThumbnailDialog extends JDialog {
         File dir = new File("screenshot");
         File[] files = dir.listFiles((d, name) -> name.endsWith(".png"));
 
-        JPanel contentPanel = new JPanel(new GridLayout(files.length / 3, 3));
-        JScrollPane scrollPane = new JScrollPane(contentPanel);
+        this.contentPanel = new JPanel();
+        this.contentPanel.setLayout(new BoxLayout(this.contentPanel, Y_AXIS));
+
+        JScrollPane scrollPane = new JScrollPane(this.contentPanel);
         scrollPane.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.addMouseWheelListener(e -> {
             JScrollBar verticalScrollBar = scrollPane.getVerticalScrollBar();
@@ -58,33 +70,59 @@ public class ThumbnailDialog extends JDialog {
         });
 
         if (files != null) {
-            Arrays.stream(files)
+            List<File> totalLlist = Arrays.stream(files)
                 .sorted(comparing(File::getName).reversed())
-                .forEach(file -> {
-                    try {
-                        BufferedImage img = ImageIO.read(file);
-                        Image thumbnailImage = img.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
-                        ImageIcon icon = new ImageIcon(thumbnailImage);
-                        JLabel label = new JLabel(icon);
+                .collect(Collectors.toList());
 
-                        label.addMouseListener(new MouseAdapter() {
-                            @Override
-                            public void mouseClicked(MouseEvent e) {
-                                imageTabPanel.addTab(file.getName());
-                                ThumbnailDialog.this.dispose();
-                            }
-                        });
+            List<File> list = totalLlist.subList(0, Math.min(9, totalLlist.size()));
+            addImage(list);
 
-                        JPanel panel = new JPanel(new BorderLayout());
-                        panel.add(label, CENTER);
-                        panel.add(new JLabel(file.getName()), SOUTH);
-                        contentPanel.add(panel);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
+            new Thread(() -> {
+                addImage(totalLlist.subList(list.size(), totalLlist.size()));
+            }).start();
         }
 
         getContentPane().add(scrollPane);
+    }
+
+    private void addImage(List<File> list) {
+        AtomicInteger atomicInteger = new AtomicInteger();
+        JPanel rowPanel = null;
+
+        for (File file : list) {
+            if (atomicInteger.getAndIncrement() % 3 == 0) {
+                rowPanel = new JPanel(new GridLayout(1, 3, 5, 5));
+                this.contentPanel.add(rowPanel);
+                this.contentPanel.revalidate();
+            }
+
+            try {
+                BufferedImage img = ImageIO.read(file);
+                Image thumbnailImage = img.getScaledInstance(300, 300, SCALE_SMOOTH);
+                ImageIcon icon = new ImageIcon(thumbnailImage);
+                JLabel label = new JLabel(icon);
+
+                label.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        imageTabPanel.addTab(file.getName());
+                        ThumbnailDialog.this.dispose();
+                    }
+                });
+
+                JPanel panel = new JPanel(new BorderLayout());
+                panel.add(label, CENTER);
+
+                JLabel fileNameLabel = new JLabel(file.getName());
+                fileNameLabel.setFont(fileNameLabel.getFont());
+                fileNameLabel.setBorder(createEmptyBorder(2, 2, 2, 2));
+                panel.add(fileNameLabel, SOUTH);
+
+                panel.setBorder(createCompoundBorder(createEtchedBorder(), createEmptyBorder(2, 2, 2, 2)));
+                rowPanel.add(panel);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
